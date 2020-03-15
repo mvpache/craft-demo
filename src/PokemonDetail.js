12b9e4c4ebdfabe586f7fb1a36c3f18eed2d81c0
mvpache <mvpache@msn.com>
2020-03-15 16:23:31 -0700

    added map to pokemon detail

--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -1,24 +1,35 @@
 import React, { Component, Fragment } from 'react';
 import axios from 'axios';
 import { withRouter } from 'react-router-dom';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import PropTypes from 'prop-types';
 
 class PokemonDetail extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat: 0,
-      long: 0,
+      locations: [],
     };
   }
 
   componentDidMount() {
-    // call the api for the map
+    const { id } = this.props.match.params;
+    axios
+      .get(`https://api.craft-demo.net/pokemon/${id}`, {
+        headers: {
+          'x-api-key': 'HHko9Fuxf293b3w56zAJ89s3IcO9D5enaEPIg86l',
+        },
+      })
+      .then(res => {
+        this.setState({ locations: res.data.locations });
+      });
   }
 
   render() {
+    console.log(this.state.locations);
     const { pokemonList } = this.props;
     const { id } = this.props.match.params;
+    const locations = this.state.locations;
     const pokemon = pokemonList.find(_pokemon => _pokemon.id == id);
     return (
       <Fragment>
@@ -42,6 +53,26 @@ class PokemonDetail extends Component {
                 heart-shaped tail.
               </p>
             </div>
+            <div>
+              <Map
+                google={this.props.google}
+                zoom={10}
+                style={{ height: '500px', width: '500px' }}
+                initialCenter={{
+                  lat: 32.819218,
+                  lng: -117.034432,
+                }}>
+                {locations.map((location, index) => {
+                  const coordinates = location.split(',');
+                  return (
+                    <Marker
+                      key={index}
+                      position={{ lat: coordinates[0], lng: coordinates[1] }}
+                    />
+                  );
+                })}
+              </Map>
+            </div>
           </div>
         ) : (
           <div />
@@ -52,7 +83,11 @@ class PokemonDetail extends Component {
   }
 }
 
-export default withRouter(PokemonDetail);
+export default withRouter(
+  GoogleApiWrapper({
+    apiKey: 'AIzaSyBICo1KGgd5Rq0t8GEbvFJnUXsnV06kcRw',
+  })(PokemonDetail)
+);
 
 PokemonDetail.propTypes = {
   pokemonList: PropTypes.array.isRequired,
